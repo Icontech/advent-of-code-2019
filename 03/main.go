c5c0158f3731899bb804c1f79dda07979af838ea
David Sandberg <david@dsandberg.com>
2019-12-10 00:49:30 +0100

    #03 Start working on second problem

--- 03/main.go
+++ 03/main.go
@@ -13,11 +13,22 @@ import (
 )
 
 func main() {
-	partOne()
+	//partOne()
+	partTwo()
 }
 
 type Point struct {
 	X, Y int64
+}
+
+type Movement struct {
+	Operation (func(Point) Point)
+	Steps     int
+}
+
+type Distance struct {
+	DistancesByPoint map[Point]int
+	CurrentDistance  *int
 }
 
 func right(startPoint Point) Point {
@@ -48,53 +59,91 @@ func down(startPoint Point) Point {
 	}
 }
 
-var movements = map[string](func(Point) Point){
+var operations = map[string](func(Point) Point){
 	"R": right,
 	"L": left,
 	"U": up,
 	"D": down,
 }
 
-func move(instructions []string) []Point {
+func move(movements []Movement) ([]Point, Distance) {
+	distance := Distance{make(map[Point]int), new(int)}
 	var allPoints []Point
-	allPoints = append(allPoints, Point{0, 0})
-	startPoint := allPoints[0]
-	for i := range instructions {
-		addedPointsAfterMovement := moveOnce(startPoint, instructions[i])
+	startPoint := Point{0, 0}
+	allPoints = append(allPoints, startPoint)
+	distance.DistancesByPoint[startPoint] = 0
+	for i := range movements {
+		addedPointsAfterMovement := moveOnce(startPoint, movements[i], distance)
 		allPoints = append(allPoints, addedPointsAfterMovement...)
 		startPoint = allPoints[len(allPoints)-1]
 	}
 
-	return allPoints
+	return allPoints, distance
 }
 
-func moveOnce(startPoint Point, input string) []Point {
-	direction := string(input[0])
-	steps, e := strconv.ParseInt(input[1:], 0, 0)
-	if e != nil {
-		log.Fatal(e)
-	}
-	movement := movements[direction]
-
+func moveOnce(startPoint Point, movement Movement, distance Distance) []Point {
 	var newPoints []Point
-	for i := 0; i < int(steps); i++ {
-		newPoints = append(newPoints, movement(startPoint))
+	for i := 0; i < movement.Steps; i++ {
+		*distance.CurrentDistance++
+		reachedPoint := movement.Operation(startPoint)
+		dist, ok := distance.DistancesByPoint[reachedPoint]
+		if ok {
+			distance.CurrentDistance = &dist
+		} else {
+			distance.DistancesByPoint[reachedPoint] = *distance.CurrentDistance
+		}
+		newPoints = append(newPoints, reachedPoint)
 		startPoint = newPoints[len(newPoints)-1]
 	}
 
 	return newPoints
 }
 
+func createMovement(instruction string) Movement {
+	direction := string(instruction[0])
+	steps, e := strconv.ParseInt(instruction[1:], 0, 0)
+	if e != nil {
+		log.Fatal(e)
+	}
+	operation := operations[direction]
+	return Movement{operation, int(steps)}
+}
+
 func partOne() {
 	fmt.Println("Part 1 start")
 
-	wire1Instructions, wire2Instructions := getInstructionsFromFile()
-	wire1Points := move(wire1Instructions)
-	wire2Points := move(wire2Instructions)
+	wire1Movements, wire2Movements := getMovementsFromFile()
+	wire1Points, _ := move(wire1Movements)
+	wire2Points, _ := move(wire2Movements)
 
 	intersections := getIntersections(wire1Points, wire2Points)
 	minimumDistance := calculateMinimumDistanceToZeroPoint(intersections)
 	fmt.Println("Manhattan distance from central port to closest intersection:", minimumDistance)
+}
+
+func partTwo() {
+	fmt.Println("Part 2 start")
+
+	wire1Movements, wire2Movements := getMovementsFromFile()
+	wire1Points, w1Dist := move(wire1Movements)
+	wire2Points, w2Dist := move(wire2Movements)
+
+	intersections := getIntersections(wire1Points, wire2Points)
+
+	minimumTotalStepsToIntersection := getShortestCombinedStepsToIntersection(intersections, w1Dist.DistancesByPoint, w2Dist.DistancesByPoint)
+	fmt.Println("Fewest combined steps the wires must take to reach an intersection:", minimumTotalStepsToIntersection)
+}
+
+func getShortestCombinedStepsToIntersection(intersections []Point, w1Distances map[Point]int, w2Distances map[Point]int) int {
+	minSteps := math.MaxInt64
+	for i := range intersections {
+		fmt.Println(intersections[i], w1Distances[intersections[i]], w2Distances[intersections[i]])
+		combinedSteps := w1Distances[intersections[i]] + w2Distances[intersections[i]]
+		if combinedSteps < minSteps {
+			minSteps = combinedSteps
+		}
+	}
+	return minSteps
 }
 
 func getIntersections(w1 []Point, w2 []Point) []Point {
@@ -131,27 +180,31 @@ func calculateMinimumDistanceToZeroPoint(points []Point) int {
 	return minDistance
 }
 
-func getInstructionsFromFile() ([]string, []string) {
+func getMovementsFromFile() ([]Movement, []Movement) {
 	file, err := os.Open("./input")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var lines [][]string
+	var wireMovements [][]Movement
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		splitLine := strings.Split(scanner.Text(), ",")
-		lines = append(lines, splitLine)
+		var movements []Movement
+		for i := range splitLine {
+			movements = append(movements, createMovement(splitLine[i]))
+		}
+		wireMovements = append(wireMovements, movements)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	if len(lines) != 2 {
+	if len(wireMovements) != 2 {
 		log.Fatal("Too many lines read from input")
 	}
 
-	return lines[0], lines[1]
+	return wireMovements[0], wireMovements[1]
 }

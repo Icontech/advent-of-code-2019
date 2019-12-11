d6cc960a4f88561162384dc4d819ff054ef8b4cc
David Sandberg <david@dsandberg.com>
2019-12-11 22:42:13 +0100

    #04 Solve second problem

--- 04/main.go
+++ 04/main.go
@@ -9,15 +9,17 @@ import (
 
 func main() {
 	partOne()
+	partTwo()
 }
+
+const startRange int = 197487
+const endRange int = 673251
 
 func partOne() {
 	fmt.Println("Part 1 start")
-	startRange := 197487
-	endRange := 673251
 	passwordCount := 0
 	for i := startRange; i <= endRange; i++ {
-		if isValidPassword(strconv.Itoa(i), 0, false) {
+		if isValidPasswordPartOne(strconv.Itoa(i), 0, false) {
 			passwordCount++
 		}
 	}
@@ -25,7 +27,7 @@ func partOne() {
 	fmt.Println("Number of different passwords:", passwordCount)
 }
 
-func isValidPassword(number string, index int, doubleFound bool) bool {
+func isValidPasswordPartOne(number string, index int, doubleFound bool) bool {
 	if index == len(number)-1 {
 		return doubleFound
 	}
@@ -42,5 +44,57 @@ func isValidPassword(number string, index int, doubleFound bool) bool {
 	}
 
 	index++
-	return isValidPassword(number, index, doubleFound)
+	return isValidPasswordPartOne(number, index, doubleFound)
+}
+
+func partTwo() {
+	fmt.Println("Part 2 start")
+	passwordCount := 0
+	for i := startRange; i <= endRange; i++ {
+		if isValidPasswordPartTwo(strconv.Itoa(i)) {
+			passwordCount++
+		}
+	}
+
+	fmt.Println("Number of different passwords:", passwordCount)
+}
+
+type DigitTracker struct {
+	Digit, Count int64
+}
+
+func isValidPasswordPartTwo(number string) bool {
+	var digitTracker DigitTracker
+	doubleFound := false
+	for i := 0; i < len(number); i++ {
+		if i == 0 {
+			firstDigit, _ := strconv.ParseInt(number[0:1], 0, 0)
+			digitTracker.Digit = firstDigit
+			digitTracker.Count = 1
+		} else {
+			currentDigit, _ := strconv.ParseInt(number[i:i+1], 0, 0)
+
+			if digitTracker.Digit > currentDigit {
+				return false
+			}
+
+			if doubleFound {
+				digitTracker.Digit = currentDigit
+			} else {
+				if digitTracker.Digit == currentDigit {
+					digitTracker.Count++
+					if i == len(number)-1 && digitTracker.Count == 2 {
+						doubleFound = true
+					}
+				} else {
+					if digitTracker.Count == 2 {
+						doubleFound = true
+					}
+					digitTracker.Digit = currentDigit
+					digitTracker.Count = 1
+				}
+			}
+		}
+	}
+	return doubleFound
 }
